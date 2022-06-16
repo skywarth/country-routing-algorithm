1aeec47e70d99d93a11b2afd1c6230e7b5f88996
skywarth <yigitk.ersoy@gmail.com>
2022-06-16 23:56:40 +0300

    testing uniqueness of certain fields, messing around

--- readme.md
+++ readme.md
@@ -13,8 +13,9 @@
 - https://github.com/amejiarosario/dsa.js-data-structures-algorithms-javascript
 - https://github.com/felipernb/algorithms.js
 - https://github.com/avoidwork/tiny-graph
-- Includes visualization, so a winner: https://github.com/strathausen/dracula
-
+- ~~Includes visualization: https://github.com/strathausen/dracula~~
+- this is such an overkill: cytoscape.js
+- https://github.com/avoidwork/tiny-graph simple yet promising
 
 ## Method:
 - Undirected graph will be applied
@@ -31,4 +32,3 @@
 
 ## Credits
 ### Resources and libraries
-- https://github.com/strathausen/dracula for graph implementation. Looking good for now, it's strong point is visualization.
--- test/page-scripts.js
+++ test/page-scripts.js
@@ -2,10 +2,41 @@ console.log(countriesDataset);
 
 
 
-//let countriesGraph = new Dracula.Graph();
+let countriesGraph = createGraph();
+
 
 
 async function insertCountriesToGraph(){
     //TODO: global vars suck, use parameter hobo
+    countriesDataset.forEach(function(country){
+        countriesGraph.addNode(country.cca3,country);
+    })
+
+    countriesGraph.forEachNode(function(node){
+        console.log(node);
+    })
+
+
+    //createUniqueLink
+    //it's defined through options
+
+
+    //TODO border links here
 
 }
+insertCountriesToGraph();
+
+
+
+
+
+function testDuplicateProperty(arr,propName){
+    //this proves we cannot use 'area' as unique identifier
+    //cca3 or similar one is better, probably.
+    let normalized=arr.map(x=>x[propName]);
+    let isDuplicate=normalized.some(function(item,index){
+        return normalized.indexOf(item) !== index
+    });
+    return isDuplicate;
+}
+
