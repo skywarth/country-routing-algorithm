94e0f44bf587ed1836fb79c775fd3463051581b2
skywarth <yigitk.ersoy@gmail.com>
2023-02-22 21:45:36 +0300

    chore: restructure and move RoutingResult to another module

--- src/country-routing-algorithm.js
+++ src/country-routing-algorithm.js
@@ -1,63 +1,9 @@
 import {NoOtherBorderException,MaxAllowedMovesAchieved} from "./exceptions.js"
 import Utils from "./utils.js"
+import RoutingResult from "./routing-result.js"
 
-class RoutingResult{
-    #foundPath=[];
-    #traversedCountries=[];
-    #isClosest=false;
-
-    #fromCountryCode='';
-    #toCountryCode='';
-
-
-    get isClosest() {
-        return this.#isClosest;
-    }
-
-    set isClosest(value) {
-        this.#isClosest = value;
-    }
-
-    get traversedCountries() {
-        return this.#traversedCountries;
-    }
-
-    get fromCountryCode() {
-        return this.#fromCountryCode;
-    }
-
-    get toCountryCode() {
-        return this.#toCountryCode;
-    }
-
-    getFoundPath(includingTheFromCountry=false) {
-        let path=[];
-        if(includingTheFromCountry){
-            path=[
-                {countryCode:this.#fromCountryCode,distanceToFinalDestination:this.pathDistance,distanceBetweenNode:0},
-
-            ]
-        }
-        path=[...path,...this.#foundPath];
-        return path;
-    }
-
-    get pathDistance() {
-        return this.#foundPath.reduce((n, {distanceBetweenNode}) => n + distanceBetweenNode, 0);
-    }
-
-    get pathCountryCount() {
-        return this.#foundPath.length;
-    }
-
-
-    constructor(foundPath, traversedCountries, fromCountryCode, toCountryCode) {
-        this.#foundPath = foundPath;
-        this.#traversedCountries = traversedCountries;
-        this.#fromCountryCode = fromCountryCode;
-        this.#toCountryCode = toCountryCode;
-    }
-}
+//maybe export RoutingResult too
+export {CountryRouting,Utils}
 
 
 class CountryRouting{
@@ -311,5 +257,3 @@ class CountryRouting{
     }
 }
 
-
-export {CountryRouting,Utils}
