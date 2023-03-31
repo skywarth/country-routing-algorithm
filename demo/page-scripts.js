776cd0d5a84d6f56af45aecfb9d83ae77b8c0c3f
skywarth <yigitk.ersoy@gmail.com>
2023-03-31 22:02:04 +0300

    chore: demonstrating git

--- demo/page-scripts.js
+++ demo/page-scripts.js
@@ -6,7 +6,7 @@ import {countriesDataset} from "../data/full.js";
 import GraphController from "../src/graph-controller.js";
 
 
-
+//simple commit to demonstrate
 
 
 let graphController=new GraphController(countriesDataset,new UndirectedGraph());
